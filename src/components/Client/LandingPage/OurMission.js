60c0c3e08e43edf1c50207c3d1aa2aafbf5b6cb6
developer yegon <gideonyegon404@gmail.com>
2024-10-26 17:34:13 +0300

    added vision text

--- src/components/Client/LandingPage/OurMission.js
+++ src/components/Client/LandingPage/OurMission.js
@@ -11,17 +11,26 @@ const OurMission = () => {
     <main className="bg-[#157FA8] py-10 px-5">
       {/* Title */}
       <div data-aos="fade-up">
-        <p className="text-5xl text-center text-[#9C54AF] font-bold mb-10">Our Mission</p>
+        <p className="md:text-5xl text-2xl text-center text-[#ffffff] font-bold mb-10">Our Mission and Vision</p>
       </div>
 
       {/* Mission Statement */}
-      <div data-aos="fade-right" className="max-w-4xl mx-auto text-white bg-[#1B4F72] p-10 rounded-lg shadow-lg">
+<div className='md:grid md:grid-cols-2 gap-2'>
+
+
+      <div data-aos="fade-right" className="max-w-4xl mx-auto text-white bg-[#1B4F72] p-5 rounded-lg shadow-lg">
         <p className="text-lg leading-8">
           Our mission is to promote responsible AI development and use, ensuring that AI is developed and used in a way that respects human rights and promotes social good. We believe that AI has the potential to transform society for the better, but only if it is developed and used ethically.
         </p>
-        <p className="mt-4 text-lg leading-8">
-          By advocating for responsible AI, we aim to ensure that AI is used to promote human rights, equality, and justice.
+       
+      </div>
+      {/* Vision Statement */}
+      <div data-aos="fade-left" className="max-w-4xl mx-auto text-white bg-[#1B4F72] p-5 rounded-lg shadow-lg mt-3 md:mt-0">
+        <p className="text-lg leading-8">
+          Our vision is a world where AI is developed and used in a way that respects human rights and promotes social good. We believe that AI has the potential to transform society for the better, but only if it is developed and used ethically.
         </p>
+       
+      </div>
       </div>
     </main>
   );
