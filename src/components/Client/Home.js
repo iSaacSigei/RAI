9724dc89b8b21f4d945a50ec2d3cf885eb5649ce
felix-whitesean <felixwhitesean@gmail.com>
2024-10-27 22:13:24 +0300

    Team and testimonials @landing page

--- src/components/Client/Home.js
+++ src/components/Client/Home.js
@@ -3,13 +3,18 @@ import FirstPage from './LandingPage/FirstPage';
 import SecondPage from './LandingPage/SecondPage';
 import Courses from './LandingPage/Courses';
 import OurMission from './LandingPage/OurMission';
+import Testimonials from './LandingPage/Testimonials';
+import Team from './LandingPage/Team';
+
 const Home = () => {
   return (
     <div>
       <FirstPage/>
       <SecondPage/>
       <Courses/>
+      <Testimonials/>
       <OurMission/>
+      <Team/>
     </div>
   )
 }
