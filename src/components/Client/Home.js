589a8ee85effb565461d8697c4a8f8a4db26632c
Duncan Langat <duncan.langat@student.moringaschool.com>
2024-11-06 16:17:28 +0300

    modify home

--- src/components/Client/Home.js
+++ src/components/Client/Home.js
@@ -1,9 +1,13 @@
 import React from 'react'
 import FirstPage from './LandingPage/FirstPage';
 import SecondPage from './LandingPage/SecondPage';
-import OurGallery from './LandingPage/SecondPage';
 import Courses from './LandingPage/Courses';
 import OurMission from './LandingPage/OurMission';
+import OurPartners from './LandingPage/OurPartners';
+import Testimonials from './LandingPage/Testimonials';
+import Team from './LandingPage/OurTeam';
+import OurGallery from './LandingPage/OurGallery';
+
 const Home = () => {
   return (
     <div>
@@ -11,7 +15,10 @@ const Home = () => {
       <SecondPage/>
       <OurGallery/>
       <Courses/>
+      <Testimonials/>
       <OurMission/>
+      <OurPartners/>
+      <Team/>
     </div>
   )
 }
