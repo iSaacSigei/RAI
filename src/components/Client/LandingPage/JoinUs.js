8ef51b666957af8ad1e6ed75196ba04aaebd5a2d
iSaacSigei <kiplangatisack.is@gmail.com>
2024-11-13 20:22:40 +1100

    Team and JoinUs page

--- src/components/Client/LandingPage/JoinUs.js
+++ src/components/Client/LandingPage/JoinUs.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import '../../../styles/joinUs.css';
+import backgroundImage from '../../../images/joinus-bg.png';
 
 const JoinUs = () => {
   return (
-    <div>JoinUs</div>
-  )
-}
+    <div 
+      className="join-us-container"
+      style={{ backgroundImage: `url(${backgroundImage})` }} // Set background image 
+    >
+      <div className="join-us-column">
+        <h2>BECOME RAI AMBASSADOR</h2>
+        <p>Join our ambassador program to promote responsible AI in communities worldwide.</p>
+        <button className='join-us-button'>Become Ambassador</button>
+      </div>
+      <div className="join-us-column">
+        <h2>BECOME RAI PARTNER</h2>
+        <p>Collaborate with us to build ethical AI initiatives that impact society positively.</p>
+        <button className='join-us-button'>Join Us as a Partner</button>
+      </div>
+    </div>
+  );
+};
 
-export default JoinUs
+export default JoinUs;
